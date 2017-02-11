e53292d67bc054eff1dbd9e1b59a0f962dd2139b
jcs <jcs@daterainc.com>
2017-02-10 23:50:40 -0800

    Enhence Dockerfile and ansible scripts

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ansible-playbook -i ./local-hosts ./build-docker-container.yml && sleep 15 \
-&& ansible-playbook -i ./local-hosts ./configure-environment.yml && sleep 5 \
-&& ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./hosts ./setup-cloudstack.yml
+ansible-playbook -i ./local-hosts ./build-docker-container.yml --become && sleep 15 \
+&& ansible-playbook -i ./local-hosts ./configure-environment.yml --become && sleep 5 \
+&& ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ./hosts ./setup-cloudstack.yml --become
